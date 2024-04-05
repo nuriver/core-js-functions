027681e7c118083f459194f230ffc772e848579c
Aleksei Golubkov <nuriver@gmail.com>
2024-04-05 18:06:27 +0200

    feat: solve getIdGeneratorFunction task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -239,8 +239,13 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let start = startFrom;
+  return function () {
+    const current = start;
+    start += 1;
+    return current;
+  };
 }
 
 module.exports = {
